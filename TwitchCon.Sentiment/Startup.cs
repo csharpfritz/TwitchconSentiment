0df645d18c9b2e832240c9f8fb141df741a4d2f3
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2019-09-24 17:05:09 -0700

    Resolving Function App root path when loading model

--- TwitchCon.Sentiment/Startup.cs
+++ TwitchCon.Sentiment/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using System;
+using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.ML;
 using TwitchCon.Sentiment;
 using TwitchCon.Sentiment.DataModels;
@@ -7,14 +9,29 @@ using TwitchCon.Sentiment.DataModels;
 namespace TwitchCon.Sentiment
 {
 
-  public class Startup : FunctionsStartup
-  {
-    public override void Configure(IFunctionsHostBuilder builder)
+    public class Startup : FunctionsStartup
     {
-      builder.Services.AddPredictionEnginePool<SentimentData, SentimentPrediction>()
-          .FromFile("MLModels/sentiment_model.zip");
-    }
-  }
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            string modelPath = Path.Combine(GetFunctionRootPath(), "MLModels/sentiment_model.zip");
+            
+            builder.Services.AddPredictionEnginePool<SentimentData, SentimentPrediction>()
+                .FromFile(modelPath);
+        }
 
+        public string GetFunctionRootPath()
+        {
+            var home = Environment.GetEnvironmentVariable("HOME");
+
+            if (home != null)
+            {
+                return Path.Combine(home, "site", "wwwroot");
+            }
+            else
+            {
+                return Environment.GetEnvironmentVariable("AzureWebJobsScriptRoot");
+            }
+        }
+    }
 }
 
